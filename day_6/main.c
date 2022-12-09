ee14c9ac9138af660f1beed5b1226eb511e8876e
Barbora Plasovska <barbora.plasovska@epita.fr>
2022-12-09 23:31:02 +0100

    day 6: done

--- day_6/main.c
+++ day_6/main.c
@@ -1,14 +1,28 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+int is_distinct(char *line, size_t j, size_t i)
+{
+    int h[26] = { 0 };
+    for (size_t a = j; a <= i; a++)
+    {
+        int c = line[a] - 'a';
+        h[c] += 1;
+        if (h[c] > 1)
+            return 0;
+    }
+    return 1;
+}
+
 size_t getmarker(char *line, size_t len)
 {
     size_t i = 0;
-    size_t count = 1;
+    size_t count = 0;
     while (i < len && line[i] != '\n')
     {
-        if (i < 3)
+        if (i < 13)
         {
             count += 1;
             i += 1;
@@ -19,16 +33,13 @@ size_t getmarker(char *line, size_t len)
             int ok = 0;
             size_t j = count > i ? 0 : i - count;
             count += 1;
-            for (; j < i; j++)
+            if (!is_distinct(line, j, i))
             {
-                if (line[i] == line[j])
-                {
-                    count = 3;
-                    ok = 1;
-                    break;
-                }
+                count = 13;
+                ok = 1;
             }
-            if (ok == 0 && count == 4)
+
+            if (ok == 0 && count == 14)
                 return i + 1;
         }
         i += 1;
